0178c9ca97336e33f793d56b588a393c5b4ad77b
ixuhan <964850509@qq.com>
2017-06-04 13:00:32 +0800

    change test

---
+++ src/run/menu.java
@@ -0,0 +1,15 @@
+/**
+* @author hank 2017/6/4 0004
+* @time 12:31:34
+* @version V1.0.0
+* @description description
+*/
+
+/** 
+* @description 
+* @param  
+* @return  
+*/
+void test(void a,void b){
+    
+}
